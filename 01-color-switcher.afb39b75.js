2a1e153f234863b4a091b26b1089b6cf3c6c3d6b
Alla199707 <38558586+Alla199707@users.noreply.github.com>
2023-02-07 17:45:11 +0000

    Deploying to gh-pages from @ Alla199707/goit-js-hw-09@cbc6d040f98922ac070ac4f2e9b87f283f2415a4 🚀

---
+++ 01-color-switcher.afb39b75.js
@@ -0,0 +1,2 @@
+let e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]"),d=null;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.afb39b75.js.map
