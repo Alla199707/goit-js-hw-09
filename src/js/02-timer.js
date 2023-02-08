3abdd497818a5b3bd7cb2e0bc321533dae4ca9b3
Alla199707 <onoschenko20@gmail.com>
2023-02-08 03:25:48 +0200

    2 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,103 @@
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+document.querySelector('body').style.backgroundColor = '#f7eff5';
+
+const input = document.getElementById('datetime-picker');
+
+const datetimePicker = document.querySelector('.timer');
+const startBtn = document.querySelector('button[data-start]');
+const seconds = document.querySelector('span[data-seconds]');
+const minutes = document.querySelector('span[data-minutes]');
+const hours = document.querySelector('span[data-hours]');
+const days = document.querySelector('span[data-days]');
+// startBtn.disabled = true;
+
+startBtn.addEventListener('click', timerStart);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      //startBtn.disabled = true;
+      Notify.failure('Пожалуйста, выберите дату будущего периода');
+    } else {
+      startBtn.disabled = false;
+      timerStart();
+      console.log(selectedDates[0]);
+      //   console.log(defaultDate);
+    }
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function timerStart() {
+  let timer = setInterval(() => {
+    let countdown = new Date(input.value) - new Date();
+    startBtn.disabled = true;
+    datetimePicker.disabled = true;
+    if (countdown >= 0) {
+      let timeObject = convertMs(countdown);
+      days.textContent = addLeadingZero(timeObject.days);
+      hours.textContent = addLeadingZero(timeObject.hours);
+      minutes.textContent = addLeadingZero(timeObject.minutes);
+      seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        datetimePicker.style.color = 'red';
+      }
+    } else {
+      datetimePicker.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+Notify.init({
+  width: '280px',
+  position: 'right-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
+  distance: '10px',
+  messageMaxLength: 110,
+  clickToClose: false,
+
+  fontFamily: 'Roboto',
+  fontSize: '13px',
+  cssAnimation: true,
+  cssAnimationDuration: 400,
+  cssAnimationStyle: 'fade', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
+  closeButton: false,
+  fontAwesomeIconSize: '34px',
+
+  failure: {
+    background: '#ff5549',
+    textColor: '#fff',
+    childClassName: 'notiflix-notify-failure',
+    notiflixIconColor: 'rgba(0,0,0,0.2)',
+    fontAwesomeClassName: 'fas fa-times-circle',
+    fontAwesomeIconColor: 'rgba(0,0,0,0.2)',
+    backOverlayColor: 'rgba(255,85,73,0.2)',
+  },
+});
